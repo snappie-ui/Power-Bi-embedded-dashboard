b8a61c1f645afae1f7041afa0003334069cd6d81
Kuldeep <kuki060605@gmail.com>
2025-12-18 10:24:07 +0530

    Fix server to use Render PORT

--- server.js
+++ server.js
@@ -46,6 +46,9 @@ app.get("/embed-config", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
+
